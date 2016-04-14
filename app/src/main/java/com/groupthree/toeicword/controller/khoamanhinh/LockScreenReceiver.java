865cf6ad60a1a5a935b7be7f96266f1c1e6b5e9c
QuyetChu <quyetchunguyen@gmail.com>
2016-04-14 22:44:58 +0700

    update

--- app/src/main/java/com/groupthree/toeicword/controller/khoamanhinh/LockScreenReceiver.java
+++ app/src/main/java/com/groupthree/toeicword/controller/khoamanhinh/LockScreenReceiver.java
@@ -4,27 +4,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.telephony.TelephonyManager;
-import android.widget.Toast;
 
 public class LockScreenReceiver extends BroadcastReceiver {
 
     Intent itLockScreenService;
     int id;
-    int key;
-    LockScreenService ls;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
         String action = intent.getAction();
         itLockScreenService = new Intent(context, LockScreenService.class);
-
-        try{
-            key = intent.getIntExtra("id2", 0);
-        }catch (Exception e){
-
-        }
-
 
         if (null != context) {
             if (action.equals(Intent.ACTION_SCREEN_OFF)) {
@@ -33,17 +23,15 @@ public class LockScreenReceiver extends BroadcastReceiver {
                 boolean isPhoneIdle = tManager.getCallState() == TelephonyManager.CALL_STATE_IDLE;
 
                 if (isPhoneIdle) {
-                    if(ls == null) {
-                        id = 1;
-                        itLockScreenService.putExtra("id", id);
-                        context.startService(itLockScreenService);
-                    }
+
+                    id = 1;
+                    itLockScreenService.putExtra("id", id);
+                    context.startService(itLockScreenService);
+
                 }
 
             }
         }
-
-        Toast.makeText(context, "abc" + key, Toast.LENGTH_SHORT).show();
 
     }
 
